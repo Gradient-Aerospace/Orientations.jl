e5930be569ceaf7aeff6fbc980611701020532f0
Tucker McClure <tucker@gradient.team>
2026-07-28 18:10:19 -0700

    Add Dimensions ext

---
+++ ext/OrientationsDimensionsExt/OrientationsDimensionsExt.jl
@@ -0,0 +1,36 @@
+module OrientationsDimensionsExt
+
+import Dimensions
+using Dimensions: RealVectorDimensionStyle, StructDimensionStyle
+using Orientations: AxisAngle, AxisAngleDeg, DirectionCosineMatrix,
+    EulerRodriguesParameters, RollPitchYaw, RollPitchYawDeg, RotationVector
+
+# Here, we expose the scalar dimensions of each of the orientation types. The simplest
+# approach is to simply treat each field of each type as a dimension. In this sense, the
+# RotationVector type would be 3-dimensional, the EulerRodriguesParameters type would be
+# 4-dimensional, and the DirectionCosineMatrix type would be 9-dimensional. Another approach
+# would be to reduce all of these to just 3 dimensions (the RotationVector type) or to
+# reduce them to 4 dimensions (EulerRodriguesParameters), since these are the only true
+# degrees of freedom (depending on exactly what you want from the notion of a "dimension" in
+# this context).
+#
+# Which should we chose? The notion of the Dimensions package isn't to reduce everything to
+# a minimal form. Rather, it's just trying to make it easy to access the nth dimension of a
+# type. In that context, it seems to make the most sense to expose the underlying data as
+# the dimensions, not the underlying meaning of an orientation. That is the desing choice
+# we've made here. Then, if you want a reduced form, simply convert to RotationVector or
+# EulerRodriguesParameters first before asking for dimensions.
+
+# These behave like structs. To get each scalar, we need to go through each field, in order.
+Dimensions.dimstyle(::Type{<:AxisAngle}) = StructDimensionStyle()
+Dimensions.dimstyle(::Type{<:AxisAngleDeg}) = StructDimensionStyle()
+Dimensions.dimstyle(::Type{<:RollPitchYaw}) = StructDimensionStyle()
+Dimensions.dimstyle(::Type{<:RollPitchYawDeg}) = StructDimensionStyle()
+# TODO: This could be made more efficient.
+
+# These behave more like vectors because they support getindex.
+Dimensions.dimstyle(::Type{<:EulerRodriguesParameters}) = RealVectorDimensionStyle()
+Dimensions.dimstyle(::Type{<:RotationVector}) = RealVectorDimensionStyle()
+Dimensions.dimstyle(::Type{<:DirectionCosineMatrix}) = RealVectorDimensionStyle()
+
+end
---
+++ test/test_dimensions_ext.jl
@@ -0,0 +1,73 @@
+module TestDimensionsExt
+
+using Test
+using StaticArrays
+using Orientations
+using Dimensions: Dimensions, dimstyle, numdims, getdim
+
+@testset "numdims for all types" begin
+
+    aa = AxisAngle(SA[1., 0., 0.], 2.)
+    aa_deg = AxisAngleDeg(SA[0., 1., 0.], 3.)
+    dcm = DirectionCosineMatrix(SA[1. 0. 0.; 0. 1. 0.; 0. 0. 1.])
+    erp = EulerRodriguesParameters(0., 0., 0., 1.)
+    rv = RotationVector(SA[1., 0., 0.])
+    rpy = RollPitchYaw(1., 2., 3.)
+    rpy_deg = RollPitchYawDeg(1., 2., 3.)
+
+    @test dimstyle(AxisAngle) == Dimensions.StructDimensionStyle()
+    @test dimstyle(AxisAngleDeg) == Dimensions.StructDimensionStyle()
+    @test dimstyle(DirectionCosineMatrix) == Dimensions.RealVectorDimensionStyle()
+    @test dimstyle(EulerRodriguesParameters) == Dimensions.RealVectorDimensionStyle()
+    @test dimstyle(RotationVector) == Dimensions.RealVectorDimensionStyle()
+    @test dimstyle(RollPitchYaw) == Dimensions.StructDimensionStyle()
+    @test dimstyle(RollPitchYawDeg) == Dimensions.StructDimensionStyle()
+
+    @test numdims(aa) == 4
+    @test numdims(aa_deg) == 4
+    @test numdims(dcm) == 9
+    @test numdims(erp) == 4
+    @test numdims(rv) == 3
+    @test numdims(rpy) == 3
+    @test numdims(rpy_deg) == 3
+
+    @test getdim(aa, 1) == 1.
+    @test getdim(aa, 2) == 0.
+    @test getdim(aa, 3) == 0.
+    @test getdim(aa, 4) == 2.
+
+    @test getdim(aa_deg, 1) == 0.
+    @test getdim(aa_deg, 2) == 1.
+    @test getdim(aa_deg, 3) == 0.
+    @test getdim(aa_deg, 4) == 3.
+
+    @test getdim(dcm, 1) == 1.
+    @test getdim(dcm, 2) == 0.
+    @test getdim(dcm, 3) == 0.
+    @test getdim(dcm, 4) == 0.
+    @test getdim(dcm, 5) == 1.
+    @test getdim(dcm, 6) == 0.
+    @test getdim(dcm, 7) == 0.
+    @test getdim(dcm, 8) == 0.
+    @test getdim(dcm, 9) == 1.
+
+    @test getdim(erp, 1) == 0.
+    @test getdim(erp, 2) == 0.
+    @test getdim(erp, 3) == 0.
+    @test getdim(erp, 4) == 1.
+
+    @test getdim(rv, 1) == 1.
+    @test getdim(rv, 2) == 0.
+    @test getdim(rv, 3) == 0.
+
+    @test getdim(rpy, 1) == 1.
+    @test getdim(rpy, 2) == 2.
+    @test getdim(rpy, 3) == 3.
+
+    @test getdim(rpy_deg, 1) == 1.
+    @test getdim(rpy_deg, 2) == 2.
+    @test getdim(rpy_deg, 3) == 3.
+
+end
+
+end
