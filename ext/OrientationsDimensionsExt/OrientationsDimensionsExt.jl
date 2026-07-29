c589d9bd00c06dc72a7260188c012220f2d13f49
Tucker McClure <tucker@gradient.team>
2026-07-28 18:54:52 -0700

    Implement entire Dimensions interface on all types

--- ext/OrientationsDimensionsExt/OrientationsDimensionsExt.jl
+++ ext/OrientationsDimensionsExt/OrientationsDimensionsExt.jl
@@ -5,40 +5,68 @@ using Dimensions: RealVectorDimensionStyle, StructDimensionStyle
 using Orientations: AxisAngle, AxisAngleDeg, DirectionCosineMatrix,
     EulerRodriguesParameters, RollPitchYaw, RollPitchYawDeg, RotationVector
 
-# Here, we expose the scalar dimensions of each of the orientation types. The simplest
-# approach is to simply treat each field of each type as a dimension. In this sense, the
-# RotationVector type would be 3-dimensional, the EulerRodriguesParameters type would be
-# 4-dimensional, and the DirectionCosineMatrix type would be 9-dimensional. Another approach
-# would be to reduce all of these to just 3 dimensions (the RotationVector type) or to
-# reduce them to 4 dimensions (EulerRodriguesParameters), since these are the only true
-# degrees of freedom (depending on exactly what you want from the notion of a "dimension" in
-# this context).
+# Here, we expose the scalar dimensions of each orientation type. The simplest approach is
+# to treat each stored scalar as a dimension. In this sense, RotationVector is
+# 3-dimensional, EulerRodriguesParameters is 4-dimensional, and DirectionCosineMatrix is
+# 9-dimensional. Another approach would be to reduce all of these to just 3 dimensions (the
+# RotationVector type) or to reduce them to 4 dimensions (EulerRodriguesParameters), since
+# these are the only true degrees of freedom (depending on exactly what you want from the
+# notion of a "dimension" in this context).
 #
-# Which should we chose? The notion of the Dimensions package isn't to reduce everything to
-# a minimal form. Rather, it's just trying to make it easy to access the nth dimension of a
-# type. In that context, it seems to make the most sense to expose the underlying data as
-# the dimensions, not the underlying meaning of an orientation. That is the desing choice
-# we've made here. Then, if you want a reduced form, simply convert to RotationVector or
-# EulerRodriguesParameters first before asking for dimensions.
+# Which should we choose? The purpose of Dimensions isn't to reduce everything to a minimal
+# form. Rather, it's to make it easy to access the nth dimension of a type. In that context,
+# it seems to make the most sense to expose the underlying data as the dimensions rather
+# than the underlying meaning of an orientation. Then, if you want a reduced form, simply
+# convert to RotationVector or EulerRodriguesParameters first.
 
-# These behave like structs. To get each scalar, we need to go through each field, in order.
+# AxisAngle dimensions are the three scalar axis components followed by the angle. Its
+# indexing behavior is intentionally different: index 1 is the complete axis.
 Dimensions.dimstyle(::Type{<:AxisAngle}) = StructDimensionStyle()
+Dimensions.numdims(::AxisAngle) = 4
+Dimensions.numdims_for_type(::Type{<:AxisAngle}) = 4
+Dimensions.getdim(aa::AxisAngle, d) = getindex((aa.axis..., aa.angle), d)
+Dimensions.eachdim(aa::AxisAngle) = (aa.axis..., aa.angle)
+
+# AxisAngleDeg has the same scalar layout as AxisAngle.
 Dimensions.dimstyle(::Type{<:AxisAngleDeg}) = StructDimensionStyle()
-Dimensions.dimstyle(::Type{<:RollPitchYaw}) = StructDimensionStyle()
-Dimensions.dimstyle(::Type{<:RollPitchYawDeg}) = StructDimensionStyle()
-# TODO: This could be made more efficient.
+Dimensions.numdims(::AxisAngleDeg) = 4
+Dimensions.numdims_for_type(::Type{<:AxisAngleDeg}) = 4
+Dimensions.getdim(aa::AxisAngleDeg, d) = getindex((aa.axis..., aa.angle), d)
+Dimensions.eachdim(aa::AxisAngleDeg) = (aa.axis..., aa.angle)
 
-# These behave more like vectors because they support getindex.
-Dimensions.dimstyle(::Type{<:EulerRodriguesParameters}) = RealVectorDimensionStyle()
-Dimensions.dimstyle(::Type{<:RotationVector}) = RealVectorDimensionStyle()
+# DirectionCosineMatrix dimensions follow its column-major linear indexing.
 Dimensions.dimstyle(::Type{<:DirectionCosineMatrix}) = RealVectorDimensionStyle()
-
-# RealVectorDimensionStyle normally uses `axes(x, 1)` to support vectors with non-traditional
-# indices. These orientation types use traditional one-based indexing but aren't
-# AbstractArrays, so access their dimensions directly rather than adding `axes` methods just
-# for Dimensions.
-Dimensions.getdim(erp::EulerRodriguesParameters, d) = getindex(erp, d)
-Dimensions.getdim(rv::RotationVector, d) = getindex(rv, d)
+Dimensions.numdims(::DirectionCosineMatrix) = 9
+Dimensions.numdims_for_type(::Type{<:DirectionCosineMatrix}) = 9
 Dimensions.getdim(dcm::DirectionCosineMatrix, d) = getindex(dcm, d)
+Dimensions.eachdim(dcm::DirectionCosineMatrix) = ntuple(d -> getindex(dcm, d), 9)
+
+# EulerRodriguesParameters dimensions follow the x, y, z, scalar indexing order.
+Dimensions.dimstyle(::Type{<:EulerRodriguesParameters}) = RealVectorDimensionStyle()
+Dimensions.numdims(::EulerRodriguesParameters) = 4
+Dimensions.numdims_for_type(::Type{<:EulerRodriguesParameters}) = 4
+Dimensions.getdim(erp::EulerRodriguesParameters, d) = getindex(erp, d)
+Dimensions.eachdim(erp::EulerRodriguesParameters) = (erp.x, erp.y, erp.z, erp.s)
+
+# RotationVector dimensions follow its x, y, z indexing order.
+Dimensions.dimstyle(::Type{<:RotationVector}) = RealVectorDimensionStyle()
+Dimensions.numdims(::RotationVector) = 3
+Dimensions.numdims_for_type(::Type{<:RotationVector}) = 3
+Dimensions.getdim(rv::RotationVector, d) = getindex(rv, d)
+Dimensions.eachdim(rv::RotationVector) = Tuple(rv.vector)
+
+# RollPitchYaw dimensions follow its roll, pitch, yaw indexing order.
+Dimensions.dimstyle(::Type{<:RollPitchYaw}) = StructDimensionStyle()
+Dimensions.numdims(::RollPitchYaw) = 3
+Dimensions.numdims_for_type(::Type{<:RollPitchYaw}) = 3
+Dimensions.getdim(rpy::RollPitchYaw, d) = getindex(rpy, d)
+Dimensions.eachdim(rpy::RollPitchYaw) = (rpy.roll, rpy.pitch, rpy.yaw)
+
+# RollPitchYawDeg has the same scalar layout as RollPitchYaw.
+Dimensions.dimstyle(::Type{<:RollPitchYawDeg}) = StructDimensionStyle()
+Dimensions.numdims(::RollPitchYawDeg) = 3
+Dimensions.numdims_for_type(::Type{<:RollPitchYawDeg}) = 3
+Dimensions.getdim(rpy::RollPitchYawDeg, d) = getindex(rpy, d)
+Dimensions.eachdim(rpy::RollPitchYawDeg) = (rpy.roll, rpy.pitch, rpy.yaw)
 
 end
--- test/test_dimensions_ext.jl
+++ test/test_dimensions_ext.jl
@@ -3,9 +3,9 @@ module TestDimensionsExt
 using Test
 using StaticArrays
 using Orientations
-using Dimensions: Dimensions, dimstyle, numdims, getdim, eachdim
+using Dimensions: Dimensions, dimstyle, numdims, getdim, eachdim, numdims_for_type
 
-@testset "numdims for all types" begin
+@testset "Dimensions interface for all types" begin
 
     aa = AxisAngle(SA[1., 0., 0.], 2.)
     aa_deg = AxisAngleDeg(SA[0., 1., 0.], 3.)
@@ -30,6 +30,23 @@ using Dimensions: Dimensions, dimstyle, numdims, getdim, eachdim
     @test numdims(rv) == 3
     @test numdims(rpy) == 3
     @test numdims(rpy_deg) == 3
+
+    # The dimensionality is fixed and available from parameterized or unparameterized types.
+    @test numdims_for_type(AxisAngle) == 4
+    @test numdims_for_type(AxisAngleDeg) == 4
+    @test numdims_for_type(DirectionCosineMatrix) == 9
+    @test numdims_for_type(EulerRodriguesParameters) == 4
+    @test numdims_for_type(RotationVector) == 3
+    @test numdims_for_type(RollPitchYaw) == 3
+    @test numdims_for_type(RollPitchYawDeg) == 3
+
+    @test numdims_for_type(typeof(aa)) == 4
+    @test numdims_for_type(typeof(aa_deg)) == 4
+    @test numdims_for_type(typeof(dcm)) == 9
+    @test numdims_for_type(typeof(erp)) == 4
+    @test numdims_for_type(typeof(rv)) == 3
+    @test numdims_for_type(typeof(rpy)) == 3
+    @test numdims_for_type(typeof(rpy_deg)) == 3
 
     @test getdim(aa, 1) == 1.
     @test getdim(aa, 2) == 0.
@@ -68,16 +85,19 @@ using Dimensions: Dimensions, dimstyle, numdims, getdim, eachdim
     @test getdim(rpy_deg, 2) == 2.
     @test getdim(rpy_deg, 3) == 3.
 
+    @test_throws BoundsError getdim(aa, 5)
+    @test_throws BoundsError getdim(aa_deg, 5)
     @test_throws BoundsError getdim(dcm, 10)
     @test_throws BoundsError getdim(erp, 5)
     @test_throws BoundsError getdim(rv, 4)
+    @test_throws BoundsError getdim(rpy, 4)
+    @test_throws BoundsError getdim(rpy_deg, 4)
 
-    # Real-vector dimension styles use the orientation itself as the dimension iterator.
+    # Each direct iterator follows the same scalar order as getdim.
     @test collect(eachdim(dcm)) == collect(dcm)
     @test collect(eachdim(erp)) == collect(erp)
     @test collect(eachdim(rv)) == collect(rv)
 
-    # Struct dimension styles flatten their indexed fields into scalar dimensions.
     @test collect(eachdim(aa)) == [1., 0., 0., 2.]
     @test collect(eachdim(aa_deg)) == [0., 1., 0., 3.]
     @test collect(eachdim(rpy)) == [1., 2., 3.]
